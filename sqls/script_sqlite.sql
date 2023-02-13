72e9d8378271296994c1d814c320b03e6e9dc5ed
tadspatrick <51645509+tadspatrick@users.noreply.github.com>
2023-02-12 22:54:55 -0300

    Montei as tabelas

---
+++ sqls/script_sqlite.sql
@@ -0,0 +1,23 @@
+CREATE TABLE IF NOT EXISTS vendedores(
+    id INTEGER PRIMARY KEY, 
+    nome TEXT NOT NULL, 
+    prontuario TEXT NOT NULL, 
+    email TEXT NOT NULL, 
+    senha TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS produtos(
+    id INTEGER PRIMARY KEY, 
+    nome TEXT NOT NULL, 
+    codigo TEXT NOT NULL,
+    valor DOUBLE NOT NULL, 
+    validade DATE NOT NULL, 
+    quantidade SIGNED BIGINT NOT NULL,
+    id_departamento FOREIGN KEY,
+);
+
+CREATE TABLE IF NOT EXISTS departamentos(
+    id INTEGER PRIMARY KEY, 
+    nome TEXT NOT NULL, 
+    responsavel TEXT NOT NULL,
+);
