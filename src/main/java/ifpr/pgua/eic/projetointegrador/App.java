4a4d206a3e4832c4a3c3a3f10d9908254a76f11f
tadspatrick <51645509+tadspatrick@users.noreply.github.com>
2023-02-14 10:27:26 -0300

    Update App.java

--- src/main/java/ifpr/pgua/eic/projetointegrador/App.java
+++ src/main/java/ifpr/pgua/eic/projetointegrador/App.java
@@ -69,7 +69,7 @@ public class App extends BaseAppNavigator {
     @Override
     public String getHome() {
         // TODO Auto-generated method stub
-        return "PRINCIPAL";
+        return "LOGIN";
     }
 
     @Override
